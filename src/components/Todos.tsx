03997ace39bec3dc4a75768dc286e8c99bc67bc6
Eduard_Bedratyi <0675060050@mail.ru>
2021-05-11 00:40:44 +0300

    feature/TP-05 using Data Model

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Todo } from '../components/models/todo';
+import { TodoItem } from './TodoItem';
 
 //React.FC is a Generic
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => {
-        return <li key={item.id}>{item.text}</li>;
+        return <TodoItem key={item.id} text={item.text} />;
       })}
     </ul>
   );
