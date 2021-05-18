f8cc7332aae0ec96f8344962aaa77dc9e144733c
Eduard_Bedratyi <0675060050@mail.ru>
2021-05-18 17:52:24 +0300

    feature/TP-10 adding styles

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Todo } from '../components/models/todo';
 import { TodoItem } from './TodoItem';
+import classes from './Todos.module.css'
 
 //React.FC is a Generic
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
-    <ul>
+    <ul className={classes.todos}>
       {props.items.map((item) => {
         return <TodoItem key={item.id} text={item.text} />;
       })}
